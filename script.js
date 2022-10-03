121597630fa8e83bd8f46698d7b4ce02cf0bbc14
Sergey <prahatuc@mail.ru>
2022-10-03 16:32:20 +0400

    changes JS

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 inputPrice.oninput = function func() {
   const num = parseInt(this.value);
-  document.getElementById("result07").innerHTML = Math.round(num + (num * 0.7));
-  document.getElementById("result06").innerHTML = Math.round(num + (num * 0.6));
-  document.getElementById("result05").innerHTML = Math.round(num + (num * 0.5));
+  const num07 = Math.round(num + (num * 0.7));
+  const num06 = Math.round(num + (num * 0.6));
+  const num05 = Math.round(num + (num * 0.5));
+  document.getElementById("result07").innerHTML = Math.round(num07 /50) * 50;
+  document.getElementById("result06").innerHTML = Math.round(num06 /50) * 50;
+  document.getElementById("result05").innerHTML = Math.round(num05 /50) * 50;
 };
 
